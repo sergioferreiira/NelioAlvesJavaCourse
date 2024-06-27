98ed7869a622cefbfdf88bf85d8500990cd582f9
Sergio Ferreira <dev.sergioferreira@gmail.com>
2024-06-27 07:16:58 -0300

    More exercises with classe and vector

--- Exercises/VectorClasses.java
+++ Exercises/VectorClasses.java
@@ -1,18 +1,66 @@
 package Exercises;
 
 public class VectorClasses {
-    private String name;
-    private int age;
-    private double height;
 
-    public VectorClasses(String name, int age, double height) {
+//    Problema "alturas"
+
+//    private String name;
+//    private int age;
+//    private double height;
+//
+//    public VectorClasses(String name, int age, double height) {
+//        this.name = name;
+//        this.age = age;
+//        this.height = height;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+
+//    Problema "soma_vetor"
+
+//    private double numbers;
+//
+//    public VectorClasses(double numbers) {
+//        this.numbers = numbers;
+//    }
+//
+//    public double getNumber(){
+//        return numbers;
+//    }
+
+//    Problema soma_2_Vetores
+
+//    private double numbers;
+//
+//    public VectorClasses(double numbers) {
+//        this.numbers = numbers;
+//    }
+//
+//    public double getNumber(){
+//        return numbers;
+//    }
+
+    private String name,email;
+    private int room;
+
+    public VectorClasses(String name, String email, int room) {
         this.name = name;
-        this.age = age;
-        this.height = height;
+        this.email = email;
+        this.room = room;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getRoom() {
+        return room;
     }
 
 }
--- Exercises/VectorProgram.java
+++ Exercises/VectorProgram.java
@@ -9,47 +9,143 @@ public class VectorProgram {
         Locale.setDefault(Locale.US);
 
 
-        int arrayNumber , minors = 0,x = 0;
-        double heightAvarage = 0;
+
+//        Problema "alturas"
+
+//        int arrayNumber , minors = 0,x = 0;
+//        double heightAvarage = 0;
+//
+//
+//
+//        System.out.print("How many persons will be typed: ");
+//        arrayNumber = sc.nextInt();
+//
+//        VectorClasses [] students = new VectorClasses[arrayNumber];
+//        String [] minorNames = new String[arrayNumber];
+//
+//        for (int i = 0 ; i < students.length; i++){
+//            System.out.printf("The data of the %d person \n" , i + 1);
+//            sc.nextLine();
+//
+//            String name = sc.nextLine();
+//            int age = sc.nextInt();
+//            double height = sc.nextDouble();
+//
+//            students[i] = new VectorClasses(name , age, height);
+//
+//            heightAvarage += height;
+//
+//            if ( age < 16){
+//
+//                minorNames[x] = students[i].getName();
+//                minors += 1;
+//                x++;
+//            }
+//
+//        }
+//
+//
+//        System.out.printf("Height Avarange : %.2f \n" , heightAvarage / arrayNumber);
+//        System.out.printf("Persons as less of 16 years old : %.1f%%\n", (double)(minors * 100) / arrayNumber);
+//        for(int i = 0; i < minors; i++){
+//            System.out.println(minorNames[i]);
+//        }
+
+
+//        Problema "soma_vetor"
+
+//        int x;
+//        double total = 0;
+//
+//        System.out.print("How many number do you will type? ");
+//        x = sc.nextInt();
+//        VectorClasses [] array = new VectorClasses[x];
+//
+//        for (int i = 0; i < array.length; i++){
+//            System.out.print("Type a number: ");
+//            double number = sc.nextInt();
+//            array[i] = new VectorClasses(number);
+//
+//        }
+//        System.out.println(" ");
+//        System.out.print("Values: ");
+//        for (int i = 0; i < array.length; i++){
+//            System.out.print(array[i].getNumber() + " " );
+//            total += array[i].getNumber();
+//        }
+//        System.out.println(" ");
+//        System.out.printf("Sum: %.2f \n" , total);
+//        System.out.printf("Average: %.2f" , (total / x));
+
+
+//        Problema soma_2_Vetores
+
+//        int x;
+//
+//        System.out.print("How many number will be there in each vector? ");
+//        x = sc.nextInt();
+//        VectorClasses [] array = new VectorClasses[x];
+//        VectorClasses [] array2 = new VectorClasses[x];
+//        VectorClasses [] array3 = new VectorClasses[x];
+//
+//        System.out.print("Type a number of the vector A \n");
+//        for (int i = 0; i < array.length; i++){
+//
+//            double number = sc.nextInt();
+//            array[i] = new VectorClasses(number);
+//
+//        }
+//        System.out.print("Type a number of the vector B \n");
+//        for (int i = 0; i < array2.length; i++){
+//            double number = sc.nextInt();
+//            array2[i] = new VectorClasses(number);
+//        }
+//
+//        for (int i = 0; i < array3.length; i++){
+//            double number = array[i].getNumber() + array2[i].getNumber();
+//            array3[i] = new VectorClasses(number);
+//        }
+//
+//        System.out.println("Vector result: ");
+//        for (int i = 0; i < array3.length; i++){
+//            System.out.println(array3[i].getNumber());
+//        }
+
+
+        String name , email;
+        VectorClasses[] roomArray = new VectorClasses[10];
+
+        System.out.print("How many students will rent rooms: ");
+        int students = sc.nextInt();
 
 
 
-        System.out.print("How many persons will be typed: ");
-        arrayNumber = sc.nextInt();
-
-        VectorClasses [] students = new VectorClasses[arrayNumber];
-        String [] minorNames = new String[arrayNumber];
-
-        for (int i = 0 ; i < students.length; i++){
-            System.out.printf("The data of the %d person \n" , i + 1);
+        for(int i= 0; i < students; i++){
             sc.nextLine();
+            System.out.printf("Student %d type your name: ", i + 1);
+            name = sc.nextLine();
 
-            String name = sc.nextLine();
-            int age = sc.nextInt();
-            double height = sc.nextDouble();
+            System.out.printf("Student %d type your email: ", i + 1);
+            email = sc.nextLine();
 
-            students[i] = new VectorClasses(name , age, height);
+            System.out.printf("Student %d chose a room: ", i + 1);
+            int roomNumber = sc.nextInt();
+            System.out.println();
 
-            heightAvarage += height;
+            roomArray[roomNumber] = new VectorClasses(name , email , roomNumber);
+        }
 
-            if ( age < 16){
-
-                minorNames[x] = students[i].getName();
-                minors += 1;
-                x++;
+        System.out.println("Busy rooms: ");
+        for(int i =0; i < roomArray.length; i++){
+            if(roomArray[i] != null){
+                System.out.println(roomArray[i].getRoom()
+                        + ": "
+                        + roomArray[i].getName()
+                        + ", "
+                        + roomArray[i].getEmail()
+                );
             }
-
         }
-
-
-        System.out.printf("Height Avarange : %.2f \n" , heightAvarage / arrayNumber);
-        System.out.printf("Persons as less of 16 years old : %.1f%%\n", (double)(minors * 100) / arrayNumber);
-        for(int i = 0; i < minors; i++){
-            System.out.println(minorNames[i]);
-        }
-
-
-
 
         sc.close();
     }
