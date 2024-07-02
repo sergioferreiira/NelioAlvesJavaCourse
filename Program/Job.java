594d75bb9f7629cac1a4dc9b49def7aa949864cb
Sergio Ferreira <dev.sergioferreira@gmail.com>
2024-07-01 21:02:05 -0300

    Exercises

--- Program/Job.java
+++ Program/Job.java
@@ -1,40 +1,40 @@
-package Program;
-
-import entities.Employee;
-
-import java.util.Locale;
-import java.util.Scanner;
-
-public class Job {
-    public static void main(String[] args){
-
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
-
-        double percent;
-        Employee x;
-        x = new Employee();
-
-
-        System.out.print("Name: ");
-        x.name = sc.nextLine();
-        System.out.print("Gross Salary: ");
-        x.grossSalary = sc.nextDouble();
-        System.out.print("Tax: ");
-        x.tax = sc.nextDouble();
-
-        System.out.println("Employee : " + x.name + " " + x.NetSalary());
-
-        System.out.println("Which percent to increase salary: ");
-        percent = sc.nextDouble();
-        x.IncreaseSalary(percent);
-
-        System.out.print("Update data: " + x.name + " , $" + x.grossSalary);
-
-
-
-
-
-        sc.close();
-    }
-}
+//package Program;
+//
+////import entities.Employee;
+//
+//import java.util.Locale;
+//import java.util.Scanner;
+//
+//public class Job {
+//    public static void main(String[] args){
+//
+//        Locale.setDefault(Locale.US);
+//        Scanner sc = new Scanner(System.in);
+//
+//        double percent;
+//        Employee x;
+//        x = new Employee();
+//
+//
+//        System.out.print("Name: ");
+//        x.name = sc.nextLine();
+//        System.out.print("Gross Salary: ");
+//        x.grossSalary = sc.nextDouble();
+//        System.out.print("Tax: ");
+//        x.tax = sc.nextDouble();
+//
+//        System.out.println("Employee : " + x.name + " " + x.NetSalary());
+//
+//        System.out.println("Which percent to increase salary: ");
+//        percent = sc.nextDouble();
+//        x.IncreaseSalary(percent);
+//
+//        System.out.print("Update data: " + x.name + " , $" + x.grossSalary);
+//
+//
+//
+//
+//
+//        sc.close();
+//    }
+//}
--- entities/Employee.java
+++ entities/Employee.java
@@ -1,14 +1,49 @@
 package entities;
 
 public class Employee {
-    public String name;
-    public double grossSalary;
-    public double tax;
 
-    public double NetSalary(){
-        return grossSalary - tax;
+    private Integer id;
+    private String name;
+    private Double salary;
+
+    public Employee() {
     }
-    public void IncreaseSalary(double percent){
-        grossSalary = grossSalary - tax + (grossSalary * (percent / 100));
+
+    public Employee(Integer id, String name, Double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public void increaseSalary(double percentage) {
+        salary += salary * percentage / 100.0;
+    }
+
+    public String toString() {
+        return id + ", " + name + ", " + String.format("%.2f", salary);
     }
 }
